79db42ec2b87f58c9e8f4a80699b2044f2545738
Cristian Díaz <crisdi384@hotmail.com>
2023-01-05 20:38:12 -0500

    Ajuste de mensaje de invitación enviada

--- src/substackForm.js
+++ src/substackForm.js
@@ -14,7 +14,7 @@ class SubstackForm extends HTMLElement {
       `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false">
       <path d="M7.5 12L10.5 15L16.5 9M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
     </svg>`;
-    this.successMessage = `¡Gracias por inscribirte! Te hemos enviado un correo de confirmación a <b>${this.inputEmail}</b> para que completes el proceso.</p>`;
+    this.successMessage = ``;
     this.attachShadow({ mode: "open" });
   }
 
@@ -246,9 +246,10 @@ class SubstackForm extends HTMLElement {
         );
 
         // Get the response and show the message in the UI
-        // let response = await request.json();
+        let response = await request.json();
+        console.log(response);
         loading.innerHTML = "";
-        announce.innerHTML = `<p>${this.successSvg} ${this.successMessage}</p>`;
+        announce.innerHTML = `<p>${this.successSvg} ¡Gracias por inscribirte! Te hemos enviado un correo de confirmación a <b>${this.inputEmail}</b> para que completes el proceso.</p>`;
         announce.classList.remove("error");
         announce.classList.add("success");
       } catch (error) {
